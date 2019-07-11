8a06b655f91c4f0fed2e6b8b75f2f8abf9022264
Rachel Kim <rayykim323@gmail.com>
2019-07-11 14:42:09 -0700

    Styling login page and menu
    Co-authored-by: adnetv <victoria.adnet@gmail.com>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,46 @@
-import React, {Component} from 'react';
-const {ipcRenderer} = require('electron');
+import React, { Component } from 'react';
+const { ipcRenderer } = require('electron');
 
 // import { connect } from 'react-redux';
 
 
-class Login extends Component{
-    constructor(props){
+class Login extends Component {
+    constructor(props) {
         super(props)
-        this.state ={
-         publicKey: '',
-         secretKey: ''   
-       }
-       this.handleChange = this.handleChange.bind(this);
-       this.handleSubmit = this.handleSubmit.bind(this);
-      }
+        this.state = {
+            publicKey: '',
+            secretKey: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    handleChange(field, fieldName){
+    handleChange(field, fieldName) {
         return this.setState(state => ({
-            [fieldName] : field
+            [fieldName]: field
         }))
     }
-    handleSubmit(publicKey, secretKey){
+    handleSubmit(publicKey, secretKey) {
         ipcRenderer.sendSync('logIn', [publicKey, secretKey]);
         this.props.logIn();
         return this.setState(state => ({
-            publicKey:'',
+            publicKey: '',
             secretKey: ''
         }))
     }
     render() {
-      console.log(this.state);
-      console.log(this.props.loginKey);
-      return (
-          <div id="loginContainer">
-              <p id='Welcome'>Mira, Mira</p>
-              <input type='text' id='publicKey' placeholder='public access key' onChange={(e)=>{this.handleChange(e.target.value, 'publicKey')}}></input>
-              <input type='text' id='secretKey' placeholder='secret access key'onChange={(e)=>{this.handleChange(e.target.value, 'secretKey')}}></input>
-              <button onClick={(e)=>{this.handleSubmit(this.state.publicKey, this.state.secretKey)}}>login</button>
-          </div>
-      )
+        let fetching = <div id="welcome"><input type="image" src="../src/assets/mira3.png" alt="Mira Logo"></input></div>
+        return (
+            <div id="loginContainer">
+                {fetching}
+                <div id='input_keys'>
+                    <div id='login_keys'><h4>Public Key</h4><input type='text' id='accessKeys' onChange={(e) => { this.handleChange(e.target.value, 'publicKey') }}></input></div>
+                    <div id='login_keys'><h4>Secret Key</h4><input type='text' id='accessKeys' onChange={(e) => { this.handleChange(e.target.value, 'secretKey') }}></input></div>
+                    <button onClick={(e) => { this.handleSubmit(this.state.publicKey, this.state.secretKey) }}>Login</button>
+                </div>
+            </div>
+        )
     }
-  }
-  
-  export default Login;
+}
+
+export default Login;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,5 @@
 //wrapper container for the entire react
-import React, {Component} from "react";
+import React, { Component } from "react";
 // import store from "../store";
 import MainContainer from "./mainContainer";
 import Menu from "../components/Menu.jsx"
@@ -19,44 +19,44 @@ const mapStateToProps = store => ({
   finishedFlag: store.graph.fetched,
   publicKey: store.login.awsPublicKey,
   privateKey: store.login.awsPrivateKey,
-  loginKey: true//store.login.loginKey
+  loginKey: store.login.loginKey
 })
 
 const mapDispatchToProps = dispatch => ({
   //get data on one single region
-    getAWSInstances: (instances) => {
-        dispatch(actions.getAWSInstances(instances));
-    },
-    //show details on specific nodes on click
-    getNodeDetails: (data) => {
-      dispatch(actions.getNodeDetails(data));
-    },
+  getAWSInstances: (instances) => {
+    dispatch(actions.getAWSInstances(instances));
+  },
+  //show details on specific nodes on click
+  getNodeDetails: (data) => {
+    dispatch(actions.getNodeDetails(data));
+  },
 
-    getAllRegions: (publicKey, privateKey) => {
-      dispatch(actions.getAllRegions(publicKey,privateKey));
-    },
-    //get credentials from folder on computer
-    getAWSKeys: (keys) => {
-      dispatch(actions.getAWSKeys(keys));
-    },
-    //login state 
-    logIn: () => {
-      dispatch(actions.logIn());
-    }
+  getAllRegions: (publicKey, privateKey) => {
+    dispatch(actions.getAllRegions(publicKey, privateKey));
+  },
+  //get credentials from folder on computer
+  getAWSKeys: (keys) => {
+    dispatch(actions.getAWSKeys(keys));
+  },
+  //login state 
+  logIn: () => {
+    dispatch(actions.logIn());
+  }
 })
 
-class App extends Component{
+class App extends Component {
 
-  render(){
+  render() {
     console.log(this.props);
     let display = [];
     display.push(<Menu publicKey={this.props.publicKey} privateKey={this.props.privateKey} getAWSInstances={this.props.getAWSInstances} currentRegion={this.props.currentRegion} getAllRegions={this.props.getAllRegions} />);
-    display.push(<MainContainer getAWSKeys={this.props.getAWSKeys} getAWSInstances={this.props.getAWSInstances} regionData={this.props.regionData} 
+    display.push(<MainContainer getAWSKeys={this.props.getAWSKeys} getAWSInstances={this.props.getAWSInstances} regionData={this.props.regionData}
       getNodeDetails={this.props.getNodeDetails} activeNode={this.props.activeNode} fetchingFlag={this.props.fetchingFlag} finishedFlag={this.props.finishedFlag}
-      edgeTable= {this.props.edgeTable}/>);
-    return(
+      edgeTable={this.props.edgeTable} />);
+    return (
       <div id="app">
-        {this.props.loginKey ? display : <Login loginKey={this.props.loginKey} logIn={this.props.logIn}/> }
+        {this.props.loginKey ? display : <Login loginKey={this.props.loginKey} logIn={this.props.logIn} />}
       </div>
     )
   }
