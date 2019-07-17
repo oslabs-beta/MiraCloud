1a967ceb2851a14fd7b74b87abb7b68b84b4553e
Victoria Adnet <victoria.adnet@gmail.com>
2019-07-16 17:00:36 -0700

    set-up create instance: region error

--- src/components/InstanceCreator.jsx
+++ src/components/InstanceCreator.jsx
@@ -2,51 +2,53 @@ import React, { Component } from "react";
 import { EC2 } from "aws-sdk";
 import RDS from "../cyto/RDS";
 const AWS = require('aws-sdk');
-
+const ec2 = new AWS.EC2();
+// import { EC2 } from "aws-sdk";
+// AWS.config.update({region: 'ap-northeast-2'});
+// let myConfig = new AWS.Config({
+//  region: "us-west-2"
+// });
+// AWS.config = myConfig
 
 class InstanceCreator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rds: false,
-      ec2: false,
-      instance: "value"
-    };
+      value: "value"
+      };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.deleteInstance = this.deleteInstance.bind(this);
     this.change = this.change.bind(this);
   }
 
   change(event) {
-    this.setState({ instance: event.target.value });
+    this.setState({ value: event.target.value });
   }
-
-  // rds() {
-  //   this.setState({ rds: true, ec2: false });
-  //   console.log(`rds => ${this.state.rds} , ec2 => ${this.state.ec2}`);
-  // }
-
-  // ec2() {
-  //   this.setState({ ec2: true, rds: false });
-  // }
 
   handleSubmit() {
     // functionality for creating rds
-    if (this.state.instance === "RDS") {
+    
+    if (this.state.value === "RDS") {
       console.log("IMMMHEREEE from rds", this.state.instance);
     }
-
+    
     // functionality for creating ec2
-    if (this.state.instance === "EC2") {
-      console.log("IMMMHEREEE from rds", this.state.instance);
+    if (this.state.value === "EC2") {
+      console.log("IMMMHEREEE from ec2", this.state.instance);
       const params = {
         ImageId: "ami-abc12345",
         InstanceType: "t2.micro",
-        KeyName: "my-key-pair",
+        // KeyName: "my-key-pair",
         MaxCount: 1,
         MinCount: 1,
-        SecurityGroupIds: ["sg-1a2b3c4d"],
-        SubnetId: "subnet-6e7f829e",
+        // SecurityGroupIds: ["sg-1a2b3c4d"],
+        // SubnetId: "subnet-6e7f829e",
+        Monitoring: {
+          Enabled: true  /* required || false */
+        },
+        Placement: {
+          AvailabilityZone: 'ap-northeast-2',
+        },
         TagSpecifications: [
           {
             ResourceType: "instance",
@@ -59,13 +61,22 @@ class InstanceCreator extends Component {
           }
         ]
       };
+      function runInstances() {
+      return new Promise((resolve, reject) =>
       ec2.runInstances(params, function (err, data) {
-        if (err) console.log(err, err.stack);
         // an error occurred
-        else console.log(data); // successful response
-      });
-    }
+        if (err) reject(err)
+        // successful response
+        resolve(data);
+      })
+    )
   }
+  runInstances()
+    .then((data) => console.log(data))
+    .catch((err) => console.log('you got an errrror=>', err))
+}
+event.preventDefault();
+}
   deleteInstance() {
     console.log(this.source.value);
     console.log(this.props.activeNode);
@@ -167,20 +178,21 @@ class InstanceCreator extends Component {
 
   render(){
     console.log('active node: ', this.props.activeNode);
-    let displayCreate = [<form>
-    <div>Create New Instances</div>
-    <select id="instance" onChange={this.change}>
-      <option value="EC2">EC2</option>
-      <option value="RDS">RDS</option>
-    </select>
-    <select id="instanceType">
-      <option value="">Instance Type 1</option>
-      <option value="">Instance Type 2</option>
-      <option value="">Instance Type 3</option>
-    </select>
-    <br />
-    <button onClick={this.handleSubmit}>Create Instance</button>
-  </form>];
+  		let displayCreate = [<form>
+        <div>Create New Instances</div>
+        <select id="instance" onChange={this.change} value={this.state.value}>
+          <option value="select">Select Instance</option>
+          <option value="EC2">EC2</option>
+          <option value="RDS">RDS</option>
+        </select>
+        <select id="instanceType">
+          <option value="">Instance Type 1</option>
+          <option value="">Instance Type 2</option>
+          <option value="">Instance Type 3</option>
+        </select>
+        <br />
+        <button onClick={this.handleSubmit}>Create Instance</button>
+      </form>];
 
   let displayDelete = [<div><h4>Selected Node:</h4>
   <select id="instance" ref={input =>(this.type = input)}>
