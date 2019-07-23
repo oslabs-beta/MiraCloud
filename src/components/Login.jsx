a34ba62ee939b450179820ca5595f59133a06fa4
Rachel Kim <rayykim323@gmail.com>
2019-07-22 23:17:30 -0700

    login page styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,15 +6,19 @@ const {ipcRenderer} = require('electron');
 
 class Login extends Component{
     constructor(props){
-        super(props)
-        this.state ={
-         publicKey: '',
-         secretKey: ''   
+      super(props)
+      this.state ={
+        publicKey: '',
+        secretKey: ''   
        }
        this.handleChange = this.handleChange.bind(this);
        this.handleSubmit = this.handleSubmit.bind(this);
       }
-
+    // componentDidMount(){
+    //   const app = document.getElementById('app');
+    //   app.style.display = 
+    //   console.log('app', app);
+    // }
     handleChange(field, fieldName){
         return this.setState(state => ({
             [fieldName] : field
@@ -29,15 +33,14 @@ class Login extends Component{
         }))
     }
     render() {
-      let fetching = <div id="welcome"><input type="image" src="../src/assets/mira3.png" alt="Mira Logo"></input></div>
       return (
-          <div id="loginContainer">
-              {fetching}
-              <div id='input_keys'>
-                <div id='login_keys'><h4>Public Key</h4><input type='text' id='accessKeys' onChange={(e)=>{this.handleChange(e.target.value, 'publicKey')}}></input></div>
-                <div id='login_keys'><h4>Secret Key</h4><input type='text' id='accessKeys' onChange={(e)=>{this.handleChange(e.target.value, 'secretKey')}}></input></div>
-                <button onClick={(e)=>{this.handleSubmit(this.state.publicKey, this.state.secretKey)}}>Login</button>
-              </div>
+          <div id="loginContainer">  
+            <div id='input_keys'>
+                <div id='keys_label'><img id="keys" src="../src/assets/key2.png"></img>Please enter your keys:</div>
+                <div id='login_keys'><input type='text' id='accessKeys' placeholder='Public Key' onChange={(e)=>{this.handleChange(e.target.value, 'publicKey')}}></input></div>
+                <div id='login_keys'><input type='text' id='accessKeys' placeholder='Secret Key' onChange={(e)=>{this.handleChange(e.target.value, 'secretKey')}}></input></div>
+                <button id='login_button' onClick={(e)=>{this.handleSubmit(this.state.publicKey, this.state.secretKey)}}>Login</button>
+            </div>
           </div>
       )
     }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,6 +4,8 @@ import React, {Component} from "react";
 import MainContainer from "./mainContainer";
 import Menu from "../components/Menu.jsx"
 import Login from "../components/Login.jsx"
+import Welcome from "../components/Welcome.jsx"
+import FlexLogin from "../components/FlexLogin.jsx"
 import * as actions from "../actions/actions.js";
 import { connect } from 'react-redux';
 
@@ -19,7 +21,7 @@ const mapStateToProps = store => ({
   finishedFlag: store.graph.fetched,
   publicKey: store.login.awsPublicKey,
   privateKey: store.login.awsPrivateKey,
-  loginKey: true //store.login.loginKey
+  loginKey: store.login.loginKey
 })
 
 const mapDispatchToProps = dispatch => ({
@@ -50,14 +52,17 @@ class App extends Component{
   render(){
     console.log(this.props);
     let display = [];
+    let loginWelcome = <FlexLogin loginKey={this.props.loginKey} logIn={this.props.logIn}/>;
     display.push(<Menu activeNode={this.props.activeNode} publicKey={this.props.publicKey} privateKey={this.props.privateKey} getAWSInstances={this.props.getAWSInstances} currentRegion={this.props.currentRegion} getAllRegions={this.props.getAllRegions} />);
     display.push(<MainContainer getAWSKeys={this.props.getAWSKeys} getAWSInstances={this.props.getAWSInstances} regionData={this.props.regionData} 
       getNodeDetails={this.props.getNodeDetails} activeNode={this.props.activeNode} fetchingFlag={this.props.fetchingFlag} finishedFlag={this.props.finishedFlag}
       edgeTable= {this.props.edgeTable} currentRegion={this.props.currentRegion}/>);
+    
     return(
       <div id="app">
-        {this.props.loginKey ? display : <Login loginKey={this.props.loginKey} logIn={this.props.logIn}/> }
+        {this.props.loginKey ? display : loginWelcome}
       </div>
+      
     )
   }
 }
